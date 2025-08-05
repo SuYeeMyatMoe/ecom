eae23b48d2a1ce3f4607e91ab29cd54af862baae
SuYeeMyatMoe <suyeemyatmoe21@gmail.com>
2025-08-05 12:59:21 +0630

    created function to direct html page in View file and create a folder called templates

--- store/views.py
+++ store/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
 
 # Create your views here.
+def home(request):#wanna pass the request in here
+    return render(request,'home.html',{})#{} is empty context dictionary
+    #home.html need to direct the template directiory in store app so create new folder (I named folder as templates to store all html templates)
