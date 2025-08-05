c76a74ebe3ef18e3911ca9ca2ee8d1d21b136c28
SuYeeMyatMoe <suyeemyatmoe21@gmail.com>
2025-08-05 13:32:16 +0630

    running comments

--- store/views.py
+++ store/views.py
@@ -4,3 +4,5 @@ from django.shortcuts import render
 def home(request):#wanna pass the request in here
     return render(request,'home.html',{})#{} is empty context dictionary
     #home.html need to direct the template directiory in store app so create new folder (I named folder as templates to store all html templates)
+    # the running will stop if terminal is close
+    #if there is no error, can run in localhost:8000
