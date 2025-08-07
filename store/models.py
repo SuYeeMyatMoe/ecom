787188df8734df93ca3799a466739c26184ace8d
SuYeeMyatMoe <suyeemyatmoe21@gmail.com>
2025-08-07 23:30:40 +0630

    Model Names

--- store/models.py
+++ store/models.py
@@ -2,12 +2,13 @@ from django.db import models
 import datetime# to know the date time and order 
 
 # Models are in here
-
+#Category
 class Category(models.Model):
     name=models.CharField(max_length=50)
     def __str__(self):
         return self.name
-    
+
+#Customer    
 class Customer(models.Model):
     first_name=models.CharField(max_length=50)
     last_name=models.CharField(max_length=50)
@@ -16,7 +17,8 @@ class Customer(models.Model):
     password=models.CharField(max_length=100)
     def __str__(self):
         return f'{self.first_name} {self.last_name} {self.phone} {self.email} {self.password}'
-    
+
+#Product    
 class Product(models.Model):
     name=models.CharField(max_length=100)
     price=models.DecimalField(default=0,decimal_places=2,max_digits=6)#can get till 100k 
@@ -26,6 +28,7 @@ class Product(models.Model):
     def __str__(self):
         return self.name
 
+#Customer Order
 class Order(models.Model):
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
     customer = models.ForeignKey(Customer, on_delete=models.CASCADE)
