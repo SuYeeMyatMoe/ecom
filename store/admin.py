1e40b6a27642dbe8858114f23856b44230423f16
SuYeeMyatMoe <suyeemyatmoe21@gmail.com>
2025-08-09 13:40:47 +0630

    After adding data and uploaded images

--- store/admin.py
+++ store/admin.py
@@ -9,5 +9,8 @@ admin.site.register(Order)
 
 #then after running again, we can see the models are added as table in http://localhost:8000/admin/
 # Then add each data
+#in product, after save and continue edit 
+# (we can see our image direction link with localhost:8000/media/uploads/product/Apple-iPhone-16-Pro-hero-240909_inline.jpg.large_2x.jpg)
+#media folder is created after upload and can find the image in there,too and also dbsqlite3 file is also updated
 
 
