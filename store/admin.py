6d9814c2439d70fda20e1db24124a79638f773d7
SuYeeMyatMoe <suyeemyatmoe21@gmail.com>
2025-08-09 13:29:37 +0630

    import and register models

--- store/admin.py
+++ store/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Category,Customer,Product,Order #import the model after migration
 
-# Register your models here.
+# After import model in above, register your models here.
+admin.site.register(Category)
+admin.site.register(Customer)
+admin.site.register(Product)
+admin.site.register(Order)
+
+
