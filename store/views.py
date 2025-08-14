0f5bead21d4602c6390755399572c246ee2b945b
SuYeeMyatMoe <suyeemyatmoe21@gmail.com>
2025-08-14 20:34:31 +0630

    get products model in home page

--- store/views.py
+++ store/views.py
@@ -3,7 +3,10 @@ from .models import Product#import Product models from Database
 
 # Create your views here.
 def home(request):#wanna pass the request in here
-    return render(request,'home.html',{})#{} is empty context dictionary
+#after importing, we will gonna use models in home page
+
+    products= Product.objects.all()#get data from product models in dbs and output as variable
+    return render(request,'home.html',{'products':products})#added model in context dictionary{} 
     #home.html need to direct the template directiory in store app so create new folder (I named folder as templates to store all html templates)
     # the running will stop if terminal is close
     #if there is no error, can run in localhost:8000
