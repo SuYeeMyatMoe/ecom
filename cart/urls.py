4e42d59491cae4604a053267c78b191251aa714b
SuYeeMyatMoe <suyeemyatmoe21@gmail.com>
2025-08-29 15:49:46 +0630

    add in views.py and create template folder in cart and create cart_summary.html file

--- cart/urls.py
+++ cart/urls.py
@@ -3,6 +3,9 @@ from . import views
 #access view file from this import
 
 urlpatterns = [
-
-    
+    #for summary,add,remove,update and will control with js ajex
+    path('',views.cart_summary, name="cart_summary"),
+    path('add/',views.cart_add, name="cart_add"),
+    path('delete/',views.cart_delete, name="cart_delete"),
+    path('update/',views.cart_update, name="cart_update"),
 ]
